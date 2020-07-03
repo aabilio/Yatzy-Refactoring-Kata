fea49e1e69903a5214508bc1fcefdf903f5bbffe
Abilio Almeida Eiroa <bili@nextail.co>
2020-07-04 00:32:35 +0200

    Replace loop with pipeline.

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -34,15 +34,17 @@ export default class Yatzy {
   }
 
   static score_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    counts[d1 - 1]++;
-    counts[d2 - 1]++;
-    counts[d3 - 1]++;
-    counts[d4 - 1]++;
-    counts[d5 - 1]++;
-    var at;
-    for (at = 0; at != 6; at++) if (counts[6 - at - 1] >= 2) return (6 - at) * 2;
-    return 0;
+    const removeItemByIndex = (arr: number[], idx: number) => arr.slice(0, idx).concat(arr.slice(idx + 1));
+    const onlyRepeated = (dice: number, idx: number, arr: number[]) => removeItemByIndex(arr, idx).indexOf(dice) >= 0;
+    const onlyUnique = (dice: number, idx: number, arr: number[]) => arr.indexOf(dice) === idx;
+    const sortDescendent = (a: number, b: number): number => b - a;
+    const getFirst = (_acc: number, _cur: number, _idx: number, arr: number[]) => arr[0];
+    const highestDiceRepeated = [d1, d2, d3, d4, d5]
+      .filter(onlyRepeated)
+      .filter(onlyUnique)
+      .sort(sortDescendent)
+      .reduce(getFirst, 0);
+    return highestDiceRepeated * 2;
   }
 
   static two_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
